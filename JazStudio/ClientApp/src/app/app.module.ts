a880307bbc50a094c53a474ceee303e0da6d9d73
John Ziemian <john.ziemian@gmail.com>
2023-08-14 12:45:36 -0400

    ok app is starting twice

--- JazStudio/ClientApp/src/app/app.module.ts
+++ JazStudio/ClientApp/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
+import { AppPartsModule } from '../library/app-parts/app-parts.module';
 
 @NgModule({
   declarations: [
@@ -26,8 +27,9 @@ import { AuthorizeInterceptor } from 'src/api-authorization/authorize.intercepto
     HttpClientModule,
     FormsModule,
     ApiAuthorizationModule,
+    AppPartsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: '', component: AppComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
     ])
