9267414cadb27cf59857a6ab09d6e37b3ecf7a5b
John Ziemian <john.ziemian@gmail.com>
2023-08-17 11:12:52 -0400

    login module

--- JazStudio/ClientApp/src/regions/welcome/welcome.module.ts
+++ JazStudio/ClientApp/src/regions/welcome/welcome.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { JzButtonsModule } from '../../library/jz-ui-controls/jz-buttons/jz-buttons.module';
+import { LoginModule } from '../../library/login/login.module';
 
 
 
@@ -11,7 +12,8 @@ import { JzButtonsModule } from '../../library/jz-ui-controls/jz-buttons/jz-butt
   ],
   imports: [
     CommonModule,
-    JzButtonsModule
+    JzButtonsModule,
+    LoginModule
   ],
   exports: [WelcomeComponent]
 })
