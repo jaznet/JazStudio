e24637df2f2af3a9549db4b3be008361bb909a15
John Ziemian <john.ziemian@gmail.com>
2023-08-19 12:48:32 -0400

    style logon

--- JazStudio/ClientApp/src/library/login/login/login.component.ts
+++ JazStudio/ClientApp/src/library/login/login/login.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      const username = this.loginForm.value.username;
+      const password = this.loginForm.value.password;
+      // Implement your login logic here
+      console.log('Username:', username);
+      console.log('Password:', password);
+    }
+  }
 }
