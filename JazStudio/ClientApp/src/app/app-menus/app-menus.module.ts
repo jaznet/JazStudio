51581e540638fb8bf23dbc2a5fab2f76c2191703
John Ziemian <john.ziemian@gmail.com>
2023-08-21 16:11:33 -0400

    ok

---
+++ JazStudio/ClientApp/src/app/app-menus/app-menus.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainMenuComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppMenusModule { }
