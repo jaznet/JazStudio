b2bdfd5a8fdc2ede77b5ea5e207740f59bbc0667
John Ziemian <john.ziemian@gmail.com>
2023-08-25 12:03:54 -0400

    ok

--- JazStudio/ClientApp/src/regions/data-viz/data-viz-menu/data-viz-menu.module.ts
+++ JazStudio/ClientApp/src/regions/data-viz/data-viz-menu/data-viz-menu.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataVizMenuComponent } from './data-viz-menu/data-viz-menu.component';
 import { JzMenusModule } from '../../../library/jz-ui-controls/jz-menus/jz-menus.module';
+import { JzButtonsModule } from '../../../library/jz-ui-controls/jz-buttons/jz-buttons.module';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { JzMenusModule } from '../../../library/jz-ui-controls/jz-menus/jz-menus
   ],
   imports: [
     CommonModule,
-    JzMenusModule
+    JzMenusModule,
+    JzButtonsModule
   ],
   exports: [
     DataVizMenuComponent
