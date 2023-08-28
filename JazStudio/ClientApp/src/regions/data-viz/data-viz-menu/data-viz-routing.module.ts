62f98a210258c1ea26b7426842653eeeb2d44ae2
John Ziemian <john.ziemian@gmail.com>
2023-08-28 12:14:18 -0400

    ok

--- JazStudio/ClientApp/src/regions/data-viz/data-viz-menu/data-viz-routing.module.ts
+++ JazStudio/ClientApp/src/regions/data-viz/data-viz-menu/data-viz-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DataVizComponent } from './data-viz.component';
+import { DataVizComponent } from '../data-viz/data-viz.component';
 import { ChorodashComponent } from '../../../views/chorodash/chorodash/chorodash.component';
 import { DataVizHomeComponent } from '../data-viz-home/data-viz-home.component';
 
--- JazStudio/ClientApp/src/regions/data-viz/data-viz.module.ts
+++ JazStudio/ClientApp/src/regions/data-viz/data-viz.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataVizComponent } from './data-viz/data-viz.component';
-import { DataVizRoutingModule } from './data-viz/data-viz-routing.module';
 import { DataVizMenuModule } from './data-viz-menu/data-viz-menu.module';
 import { DataVizHomeComponent } from './data-viz-home/data-viz-home.component';
 import { RouterModule } from '@angular/router';
-
-
+import { DataVizRoutingModule } from './data-viz-menu/data-viz-routing.module';
 
 @NgModule({
   declarations: [
