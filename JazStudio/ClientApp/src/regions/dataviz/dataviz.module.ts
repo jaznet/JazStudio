8e6b6051a5667e566f35ccc301e37cf04ee2b991
John Ziemian <john.ziemian@gmail.com>
2023-08-29 11:07:55 -0400

    rename dataviz

--- JazStudio/ClientApp/src/regions/dataviz/dataviz.module.ts
+++ JazStudio/ClientApp/src/regions/dataviz/dataviz.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataVizHomeComponent } from './data-viz-home/data-viz-home.component';
 import { RouterModule } from '@angular/router';
-import { DataVizRoutingModule } from './data-viz-nav/data-viz-routing.module';
+import { DataVizRoutingModule } from './data-viz-nav/dataviz-routing.module';
 import { JzButtonsModule } from '../../library/jz-ui-controls/jz-buttons/jz-buttons.module';
 import { JzMenusModule } from '../../library/jz-ui-controls/jz-menus/jz-menus.module';
 import { DatavizViewComponent } from './dataviz-view/dataviz-view.component';
