71863e66fc5915ab1de3a7859309728877dcd713
John Ziemian <john.ziemian@gmail.com>
2023-08-29 12:50:12 -0400

    backoffice-view works!

--- JazStudio/ClientApp/src/regions/backoffice/backoffice-view/backoffice-view.component.ts
+++ JazStudio/ClientApp/src/regions/backoffice/backoffice-view/backoffice-view.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostBinding, OnInit } from '@angular/core';
+import { JzRegionBase } from '../../../library/jz-regions/jz-region-base';
+import { AppService } from '../../../app/app.service';
 
 @Component({
   selector: 'app-backoffice-view',
   templateUrl: './backoffice-view.component.html',
   styleUrls: ['./backoffice-view.component.css']
 })
-export class BackofficeViewComponent {
+export class BackofficeViewComponent extends JzRegionBase implements OnInit, AfterViewInit {
 
+  @HostBinding('class') classes = 'fit-to-parent';
+
+  constructor(private host: AppService) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.host.showMenu();
+    console.log("BackofficeViewComponent");
+  }
+
+  ngAfterViewInit(): void {
+    this.host.showMenu();
+    console.log('Method not implemented.');
+  }
 }
