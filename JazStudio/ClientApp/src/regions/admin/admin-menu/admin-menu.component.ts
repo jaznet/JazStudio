9d9d654f1394340607ae27fdb69eff572c293eff
John Ziemian <john.ziemian@gmail.com>
2023-08-30 15:48:47 -0400

    menus

--- JazStudio/ClientApp/src/regions/admin/admin-menu/admin-menu.component.ts
+++ JazStudio/ClientApp/src/regions/admin/admin-menu/admin-menu.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-admin-menu',
+  selector: 'admin-menu',
   templateUrl: './admin-menu.component.html',
   styleUrls: ['./admin-menu.component.css']
 })
--- JazStudio/ClientApp/src/regions/architecture/architecture-menu/architecture-menu.component.ts
+++ JazStudio/ClientApp/src/regions/architecture/architecture-menu/architecture-menu.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-architecture-menu',
+  selector: 'architecture-menu',
   templateUrl: './architecture-menu.component.html',
   styleUrls: ['./architecture-menu.component.css']
 })
