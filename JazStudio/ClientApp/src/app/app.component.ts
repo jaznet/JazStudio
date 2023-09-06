22aa241a1cca07a54fee429c497a8743ed86b290
John Ziemian <john.ziemian@gmail.com>
2023-09-06 09:04:23 -0400

    ok

--- JazStudio/ClientApp/src/app/app.component.ts
+++ JazStudio/ClientApp/src/app/app.component.ts
@@ -4,7 +4,8 @@ import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   @HostBinding('class') classes = 'fit-to-parent';
