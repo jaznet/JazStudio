d37b6ed35c40f5700d767ab8b7adb27c7aa832c6
John Ziemian <john.ziemian@gmail.com>
2023-09-07 11:24:39 -0400

    sync dashboard

--- JazStudio/ClientApp/src/views/sync-dash/sync-dash/sync-dash.component.ts
+++ JazStudio/ClientApp/src/views/sync-dash/sync-dash/sync-dash.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { DashboardLayoutComponent } from '@syncfusion/ej2-angular-layouts';
+import { AfterViewInit, Component, HostBinding, OnInit, ViewChild } from '@angular/core';
+import { DashboardLayoutComponent, PanelModel } from '@syncfusion/ej2-angular-layouts';
 import { registerLicense } from '@syncfusion/ej2-base';
 import { SyncLicService } from '../../../services/app-services/sync-lic.service';
 
@@ -10,12 +10,15 @@ import { SyncLicService } from '../../../services/app-services/sync-lic.service'
   styleUrls: ['./sync-dash.component.css']
 })
 export class SyncDashComponent implements OnInit, AfterViewInit {
+  @HostBinding('class') classes = 'fit-to-parent';
+  @ViewChild('default_dashboard')
 
-  constructor(private sync: SyncLicService) {
+  public dashboard!: DashboardLayoutComponent;
+  public count: number = 8;
+  public cellSpacing: number[] = [10, 10];
 
-  }
+  constructor(private sync: SyncLicService) { }
   
-
   ngOnInit(): void {
     registerLicense('Ngo9BigBOggjHTQxAR8/V1NGaF1cWGhIfEx1RHxQdld5ZFRHallYTnNWUj0eQnxTdEZjUH9ZcH1RQGFeVEdxXQ==');
   }
@@ -24,4 +27,20 @@ export class SyncDashComponent implements OnInit, AfterViewInit {
     this.sync.HideSyncWarning();
   }
 
+  addPanel(): void {
+    let panel: PanelModel[] = [{
+      'id': this.count.toString(), 'sizeX': 1, 'sizeY': 1, 'row': 0, 'col': 0,
+      content: '<span id="close" class="e-template-icon e-close-icon"></span><div class="text-align">' + this.count.toString() + '</div>'
+    }];
+    this.dashboard.addPanel(panel[0]);
+    let closeIcon: any = document.getElementById(this.count.toString())!.querySelector!('.e-close-icon');
+    closeIcon.addEventListener('click', this.onCloseIconHandler.bind(this));
+    this.count = this.count + 1;
+  }
+
+  onCloseIconHandler(event: any): void {
+    if ((<HTMLElement>event.target).offsetParent) {
+      this.dashboard.removePanel((<HTMLElement>event.target).offsetParent!.id);
+    }
+  }
 }
