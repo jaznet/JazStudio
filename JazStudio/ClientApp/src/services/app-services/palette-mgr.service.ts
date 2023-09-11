28c9024e27e9bd6de3bb1bd422a2251418934530
John Ziemian <john.ziemian@gmail.com>
2023-09-11 09:49:13 -0400

    ok

--- JazStudio/ClientApp/src/services/app-services/palette-mgr.service.ts
+++ JazStudio/ClientApp/src/services/app-services/palette-mgr.service.ts
@@ -28,7 +28,7 @@ export class PaletteMgrService {
         document.documentElement.style.setProperty('--jz-palette-color-5', '#ffffdb');
 
         document.documentElement.style.setProperty('--jz-palette-border', 'var(--jz-palette-color-3');
-        document.documentElement.style.setProperty('--jz-palette-text-1', 'var(--jz-palette-color-3');
+        document.documentElement.style.setProperty('--jz-palette-text-1', '#CBCBB0');
         document.documentElement.style.setProperty('--jz-palette-text-2', '#D7AE46');
         document.documentElement.style.setProperty('--jz-palette-text-3', 'var(--jz-palette-color-5');
         document.documentElement.style.setProperty('--jz-palette-text-4', 'black');
