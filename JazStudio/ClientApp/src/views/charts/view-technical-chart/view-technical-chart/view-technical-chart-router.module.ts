0d5d5e226a6225843b1078247ad7e3345457403a
John Ziemian <john.ziemian@gmail.com>
2023-09-19 11:32:22 -0400

    ok

--- JazStudio/ClientApp/src/views/charts/view-technical-chart/view-technical-chart/view-technical-chart-router.module.ts
+++ JazStudio/ClientApp/src/views/charts/view-technical-chart/view-technical-chart/view-technical-chart-router.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { JzTechnicalChartComponent } from '../../../../library/jz-components/jz-charts/jz-technical-chart/jz-technical-chart.component';
+import { ViewTechnicalChartComponent } from './view-technical-chart.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: JzTechnicalChartComponent
+    component: ViewTechnicalChartComponent
   }
 ];
 
