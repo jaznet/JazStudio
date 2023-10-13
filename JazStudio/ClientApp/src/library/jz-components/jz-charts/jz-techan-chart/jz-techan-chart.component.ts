c00b94d6d68633cb867420ed5db2520a73dd8bd0
John Ziemian <john.ziemian@gmail.com>
2023-10-13 17:44:46 -0400

    ok

--- JazStudio/ClientApp/src/library/jz-components/jz-charts/jz-techan-chart/jz-techan-chart.component.ts
+++ JazStudio/ClientApp/src/library/jz-components/jz-charts/jz-techan-chart/jz-techan-chart.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, HostBinding, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostBinding, OnInit } from '@angular/core';
 import * as d3 from 'd3';
 import * as techan from 'techan';
 
@@ -9,12 +9,28 @@ import * as techan from 'techan';
 })
 export class JzTechanChartComponent implements OnInit, AfterViewInit {
   @HostBinding('class') classes = 'fit-to-parent';
+  private chartData: any[]; // Your OHLC data
+
+
+  constructor(private elementRef: ElementRef) {
+    this.chartData = [/* Load your OHLC data here */];
+  }
 
     ngOnInit(): void {
-       
+      // Initialize D3 and Techan after ensuring that D3 is ready.
+      d3.select(this.elementRef.nativeElement).append('svg').attr('width', 500).attr('height', 500);
+
+      // Now, you can initialize Techan and set up your chart.
+    //  const xScale = techan.scale.financetime().range([0, 100]);;
     }
     ngAfterViewInit(): void {
-      //var techanTime = techan.scale.financetime().range([0, 100]);
+      const container = this.elementRef.nativeElement.querySelector('.chart-container');
+
+      // Create and render the chart in the 'container' element using D3.js and Techan.
+      const chart = this.createTechanChart(container, this.chartData);
     }
 
+  createTechanChart(container: any, chartData: any[]) {
+    console.log('createTechanChart');
+  }
 }
